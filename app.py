4b77552242cce2bbededb055acf64c000b519685
Sarah Fink <sarah.fink@online.de>
2024-05-24 12:36:34 +0200

    update

--- app.py
+++ app.py
@@ -8,16 +8,35 @@ import datetime
 '''
 Enter your fare information
 '''
+
+def get_lat_lon(adress):
+    url = "https://nominatim.openstreetmap.org/search"
+    params={
+        "q": f"{adress} New York",
+        "format": "json"
+    }
+    response = requests.get(url, params=params).json()
+    lat = response[0]["lat"]
+    lon = response[0]["lon"]
+    return float(lat), float(lon)
+
+location_columns = st.columns(2)
+pickup = location_columns[0].text_input("Fill in your pickup location", "Manhattan")
+pickup_lat, pickup_lon = get_lat_lon(pickup)
+
+dropoff = location_columns[1].text_input("Fill in your dropoff location", "Upper West Side")
+dropoff_lat, dropoff_lon = get_lat_lon(dropoff)
+
 date = st.date_input(
     "Day",
     datetime.date(2019, 7, 6))
 
 time = st.time_input('Time', datetime.time(8, 45))
 date_time = datetime.datetime.combine(date, time).strftime('%Y-%m-%d %H:%M:%S')
-pickup_lon = st.number_input('Fill in pickup longitude', min_value=-90.0, max_value=90.0, value=-73.950655)
-pickup_lat = st.number_input('Fill in pickup latitude', min_value=-90.0, max_value=90.0, value= 40.783282)
-dropoff_lon = st.number_input('Fill in dropoff longitude', min_value=-90.0, max_value=90.0, value= -73.984365)
-dropoff_lat = st.number_input('Fill in dropoff latitude', min_value=-90.0, max_value=90.0, value= 40.769802)
+# pickup_lon = st.number_input('Fill in pickup longitude', min_value=-90.0, max_value=90.0, value=-73.950655)
+# pickup_lat = st.number_input('Fill in pickup latitude', min_value=-90.0, max_value=90.0, value= 40.783282)
+# dropoff_lon = st.number_input('Fill in dropoff longitude', min_value=-90.0, max_value=90.0, value= -73.984365)
+# dropoff_lat = st.number_input('Fill in dropoff latitude', min_value=-90.0, max_value=90.0, value= 40.769802)
 passenger_count = st.number_input('Fill in passenger count', min_value=1, max_value=100, value= 1)
 
 url = 'https://taxifare.lewagon.ai/predict'
